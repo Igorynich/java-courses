a80f031fb9122d96b6765ffd4385658775cbafbb
User <C:\Users\User\AppData\Roaming\The Bat!>
2017-02-12 16:07:18 +0300

    added diff(-) and multi(*)

--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,16 @@ public class Calculator {
 		}
 	}	
 
+	public void diff(int param1, int param2) {
+		this.result = param1 - param2;
+	}
+
+	public void multi(int param1, int param2) {
+		this.result = param1 * param2;
+		/*for (Integer param : params) {
+			this.result *= param;
+		}*/
+	}
 	public int getResult() {
 		return this.result;
 	}
--- InteractRunner.java
+++ InteractRunner.java
@@ -12,9 +12,24 @@ public class InteractRunner {
 				String first = reader.next();
 				System.out.println("Enter the second arg: ");
 				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
-				System.out.println("Result: " + calc.getResult());
-				calc.cleanResult();
+				System.out.println("Enter the operation: +/-/*: ");
+				switch (reader.next()) {
+					case "+" : calc.add(Integer.valueOf(first), Integer.valueOf(second));
+						System.out.println("Result: " + calc.getResult());
+						calc.cleanResult();
+						break;
+					case "-" : calc.diff(Integer.valueOf(first), Integer.valueOf(second));
+						System.out.println("Result: " + calc.getResult());
+						calc.cleanResult();
+						break;
+					case "*" : calc.multi(Integer.valueOf(first), Integer.valueOf(second));
+						System.out.println("Result: " + calc.getResult());
+						calc.cleanResult();
+						break;
+					default : System.out.println("Operation is not supported");
+					break;
+				}
+
 				System.out.println("Exit: yes/no");
 				exit=reader.next();
 			}
